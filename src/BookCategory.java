31327c787ba31d83b194f47093ee94c02412bcc2
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-26 11:13:28 +0200

    Book

--- src/BookCategory.java
+++
@@ -1,4 +0,0 @@
-public enum BookCategory {
-    TITLE, GENRE, AUTHOR;
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         TryCatch tryCatch1 = new TryCatch();
-        UserMenu userMenu = new UserMenu();
+        Book book1 = new Book("The Green Mile","Steven King", "Thriller", true);
         //----------------------------------------
 
 
         System.out.println("Welcome! \nWould you like to login or register a new account? \n1. Login \n2. Register an account \n3. Exit ");
         int choice =  tryCatch1.tryCatch1();
         switch(choice) {
-            case 1 -> userMenu.userMenu();
-            case 2 ->
+            case 1 -> System.out.println("hej");
+            case 2 -> System.out.println("hej");
             case 3 -> System.exit(0); // vil kolalr vad detta är
         }
     }
