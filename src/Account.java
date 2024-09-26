f28aa8b50e8cc4cf0977d984f4d4707073622b32
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-26 13:09:08 +0200

    nåt fel

--- src/Account.java
+++ src/Account.java
@@ -6,23 +6,35 @@ public class Account {
     ArrayList<User> users = new ArrayList<User>();
 
 
-    public void testLäggTill () {
+    public void testLäggTill() {
         users.add(new User("Oliwer", "Lösen"));
         users.add(new User("Karolina", "Lösen768"));
         users.add(new User("Mergim", "LösenhejHå"));
     }
+
     //----------------METHODS for user-------------
-    public void createAccount () {
+    public void createAccount() {
         System.out.println("please enter username");
         String username = tryCatch.tryCatchAccountCreation();
         System.out.println("please enter password");
         String password = tryCatch.tryCatchAccountCreation();
-        users.add(new User(username,password));
+        users.add(new User(username, password));
         System.out.println("You have successfully created a new account with username " + username + " and password " + password);
     }
-    public void loginCheck () {
 
+    public void loginCheck() {
+        while (true) {
+            String usernameToTest = tryCatch.tryCatchAccountCreation();
+            String passwordToTest = tryCatch.tryCatchAccountCreation();
+            if (users.contains(usernameToTest) && users.contains(passwordToTest)) {
+                System.out.println("You have successfully logged in");
+                break; // men ändra till att vi ska komam till meny här ifrån ifall vi lyckas logga in
+            } else {
+                System.out.println("You have unsuccessfully logged in");
+            }
+        }
     }
+
     //--------------METHODS for Lib --------------------------
     public void displayAllUser() {
         for (User user : users) {
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         TryCatch tryCatch1 = new TryCatch();
         Account account1 = new Account();
-        Book book1 = new Book("The Green Mile","Steven King", "Thriller", true);
+        Book book1 = new Book("The Green Mile", "Steven King", "Thriller", true);
         //----------------------------------------
         User user1 = new User("Oliwer", "Lösen");
         account1.testLäggTill();
@@ -10,15 +10,15 @@ public class Main {
 
         //tester
         while (true) {
-        System.out.println("Welcome! \nWould you like to login or register a new account? \n1. Login \n2. Register an account \n3. Exit \n4. print users");
-        int choice =  tryCatch1.tryCatch1();
-        switch(choice) { // denna ska göras om, jsut nu bara för testning
-            case 1 -> book1.displayBookDetails();
-            case 2 -> account1.createAccount();
-            case 3 -> System.exit(0); // vil kolalr vad detta är
-            case 4 -> account1.displayAllUser();
+            System.out.println("Welcome! \nWould you like to login or register a new account? \n1. Login \n2. Register an account \n3. Exit \n4. print users");
+            int choice = tryCatch1.tryCatch1();
+            switch (choice) { // denna ska göras om, jsut nu bara för testning
+                case 1 -> book1.displayBookDetails();
+                case 2 -> account1.createAccount();
+                case 3 -> System.exit(0); // vil kolalr vad detta är
+                case 4 -> account1.displayAllUser();
 
-        }
+            }
         }
     }
 }
